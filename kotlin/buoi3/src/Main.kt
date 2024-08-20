e89b43c9dfa57fe7869eccd657ce7f163547f5d5
hungvo29122003 <acczinsv5@gmail.com>
2024-08-19 19:49:28 -0700

    update buoi 3

--- kotlin/buoi3/src/Main.kt
+++ kotlin/buoi3/src/Main.kt
@@ -109,53 +109,146 @@
 //    manage.addAnimal(dog)
 //    manage.showInfo()
 //}
+//
+//open  class Shape(){
+//    private var name : String? = null
+//
+//    fun setInfo(newName : String){
+//        this.name = newName
+//    }
+//    open fun calculater() : Double{
+//        return 0.0
+//    }
+//}
+//
+//class Cricle() : Shape(){
+//    private var radius : Double = 0.0
+//
+//    fun setInfo(newName: String, newRadius : Double){
+//        super.setInfo(newName = newName)
+//        this.radius = newRadius
+//    }
+//    override fun calculater() : Double{
+//        return Math.PI * radius * radius
+//    }
+//}
+//
+//class Rectangle() : Shape(){
+//    private var width : Double = 0.0
+//    private var height : Double = 0.0
+//
+//    fun setInfo(newName: String, newWith: Double, newHeight : Double){
+//        super.setInfo(newName = newName)
+//        this.width = newWith
+//        this.height = newHeight
+//    }
+//
+//    override fun calculater() : Double{
+//        return width * height
+//    }
+//
+//}
+//
+//fun main() {
+//    val cricle = Cricle()
+//    val rectangle = Rectangle()
+//    cricle.setInfo("hình tròn", 5.0)
+//    rectangle.setInfo("hình chữ nhật", 4.0, 3.0)
+//    println("Diện tích của hình tròn là ${cricle.calculater()}" )
+//    println("Diện tích của hình chữ nhật là ${rectangle.calculater()}")
+//}
+//
 
-open  class Shape(){
-    private var name : String? = null
+open class Person(){
+    private var name: String? = null
+    private var age : Int? = null
+    private var address : String? = null
 
-    fun setInfo(newName : String){
+    fun setInfo(newName: String, newAge : Int, newAddress : String){
         this.name = newName
+        this.age = newAge
+        this.address = newAddress
     }
-    open fun calculater() : Double{
-        return 0.0
+    open fun getInfo() : String{
+        return "name $name age $age address $address"
+    }
+    fun eat(){
+        println("$name đang ăn")
+    }
+
+    fun sleep(){
+        println("$name đang ngủ")
+    }
+
+    open fun work() : String{
+        return ("$name đang làm việc")
+    }
+}
+class Student() : Person(){
+    var students : String? = null
+
+    fun setInfo(newName: String, newAge: Int, newAddress: String, newStudents: String){
+        super.setInfo(newName = newName, newAge = newAge, newAddress = newAddress)
+        this.students = newStudents
+    }
+    override fun work() : String{
+        return ("${super.getInfo()}, đang học tập ở $students")
     }
 }
 
-class Cricle() : Shape(){
-    private var radius : Double = 0.0
-
-    fun setInfo(newName: String, newRadius : Double){
-        super.setInfo(newName = newName)
-        this.radius = newRadius
+class Teacher() : Person(){
+    private var subject : String? = null
+    fun setInfo(newName: String, newAge: Int, newAddress: String, newSubject: String){
+        super.setInfo(newName = newName, newAge = newAge, newAddress = newAddress)
+        this.subject = newSubject
     }
-    override fun calculater() : Double{
-        return Math.PI * radius * radius
+    override fun work() : String{
+        return ("${super.getInfo()} đang dạy môn $subject")
     }
 }
 
-class Rectangle() : Shape(){
-    private var width : Double = 0.0
-    private var height : Double = 0.0
+class Engineer() : Person(){
+    private var company: String? = null
 
-    fun setInfo(newName: String, newWith: Double, newHeight : Double){
-        super.setInfo(newName = newName)
-        this.width = newWith
-        this.height = newHeight
+    fun setInfo(newName: String, newAge: Int, newAddress: String, newCompany: String){
+        super.setInfo(newName = newName, newAge = newAge, newAddress = newAddress)
+        this.company = newCompany
     }
 
-    override fun calculater() : Double{
-        return width * height
+    override  fun work() : String{
+        return ("${super.getInfo()}, đang làm tại công ty $company")
+    }
+}
+
+class PersonManage(){
+    private val listWork = mutableListOf<Person>()
+
+    fun addWord(person: Person){
+        listWork.add(person)
     }
 
+    fun remoteWord(person: Person){
+        listWork.remove(person)
+    }
+    fun showWord(){
+        listWork.forEach(){
+            println(it.work())
+        }
+    }
 }
 
 fun main() {
-    val cricle = Cricle()
-    val rectangle = Rectangle()
-    cricle.setInfo("hình tròn", 5.0)
-    rectangle.setInfo("hình chữ nhật", 4.0, 3.0)
-    println("Diện tích của hình tròn là ${cricle.calculater()}" )
-    println("Diện tích của hình chữ nhật là ${rectangle.calculater()}")
+    val personManage = PersonManage()
+    val student = Student()
+    val teacher = Teacher()
+    val engineer = Engineer()
+    student.setInfo(newName = "Hùng", newAge = 20, newAddress = "Gia lai", newStudents = "Gia định")
+    teacher.setInfo(newName = "Hùng", newAge = 21, newAddress = "Gia lai", newSubject = "android dev")
+    engineer.setInfo(newName = "Hùng", newAge = 22, newAddress = "Gia lai", newCompany = "ZERO")
+    personManage.addWord(student)
+    personManage.addWord(teacher)
+    personManage.addWord(engineer)
+    println("-------------------------------------------------")
+    personManage.showWord()
+    println("-------------------------------------------------")
 }
-
-
