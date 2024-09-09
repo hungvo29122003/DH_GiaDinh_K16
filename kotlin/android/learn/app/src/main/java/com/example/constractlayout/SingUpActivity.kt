3537e1430601fe506fe3f23a2494f3e7f173620e
hungvo29122003 <acczinsv5@gmail.com>
2024-09-09 02:03:00 -0700

    update layout

--- kotlin/android/learn/app/src/main/java/com/example/constractlayout/SingUpActivity.kt
+++ kotlin/android/learn/app/src/main/java/com/example/constractlayout/SingUpActivity.kt
@@ -1,28 +1,21 @@
 package com.example.constractlayout
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.constractlayout.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+class SingUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_sing_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.tvSignUp.setOnClickListener{
-            val intent = Intent(this, SingUpActivity::class.java)
-            startActivity(intent)
-        }
+
     }
 }
