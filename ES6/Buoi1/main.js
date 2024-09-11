48b4b0728ae74b0b615626121c0eadf0e9978b2b
hungvo29122003 <acczinsv5@gmail.com>
2024-09-11 00:43:31 -0700

    update

---
+++ ES6/Buoi1/main.js
@@ -0,0 +1,19 @@
+// var a = 100;
+// if(a > 50 ) 
+// {
+//     var kq = "so lon hon 50"
+//     console.log(kq)
+// } else{
+//     var kq = "so nho hon 50"
+//     console.log(kq)
+// }
+// console.log(a)
+// console.log(kq)
+// LET CHI CO TAC DUNG trong khoi block\
+let a = 100
+if(a > 50) {
+    let kq = "so lon hon 50"
+    console.log(kq)
+}
+console.log(a)
+console.log(kq)
