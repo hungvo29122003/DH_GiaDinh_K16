33fd8668a8cf16f8285625f722649231171b2d39
hungvo29122003 <acczinsv5@gmail.com>
2024-10-03 01:01:34 +0700

    update javaFX

--- Java_App/Lap_Trinh_Ung_Dung_Vs_Java/CalSceneBuild/src/application/ViewCalculater.java
+++ Java_App/Lap_Trinh_Ung_Dung_Vs_Java/CalSceneBuild/src/application/ViewCalculater.java
@@ -5,8 +5,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
 
 public class ViewCalculater {
 	// phương thức hiển thị kết quả sẽ hiển thị ketQua lên textield
